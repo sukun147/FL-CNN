16706cf8e144a7bb2e456f2393bab90a67bea7e2
mylRalph <95121572+mylRalph@users.noreply.github.com>
2022-05-28 21:26:52 +0800

    Update and rename sever.py to server.py
    修改了server处的一点小问题

--- demo/server.py
+++ demo/server.py
@@ -5,19 +5,16 @@ epoch = 100
 client_num = 2
 host = ''
 port = 666
-sever = conn.Server(host, port, client_num)
+server = conn.Server(host, port, client_num)
 
 for i in range(epoch):
     params_list = []
     # 接收客户端发送过来的参数
-    for j in range(client_num):
-        if sever.accept():
-            params_list.append(sever.recv())
-            print("111111")
+    for i in server.recv:
+        params_list.append(i)
 
     # 参数聚合
     avg_params = utils.model_average(params_list)
 
     # 分发参数
-    for j in range(client_num):
-        sever.send(avg_params)
+    server.send(avg_params)
